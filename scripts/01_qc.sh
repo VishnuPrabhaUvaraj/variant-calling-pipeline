25d1e28e2cb65c353dcec858c348347035f41f77
VishnuPrabhaUvaraj <vpuva2003@gmail.com>
2026-03-30 12:56:18 +0530

    Phase 2 complete: QC pipeline - FastQC and Trimmomatic done

--- scripts/01_qc.sh
+++ scripts/01_qc.sh
@@ -6,7 +6,7 @@ RAW_R2="data/raw/NA12878_R2.fastq.gz"
 TRIMMED_DIR="data/trimmed"
 QC_DIR="results/qc"
 THREADS=4
-ADAPTER="ADAPTER_PATH_HERE"
+ADAPTER="/home/vishnuprabha/miniconda3/envs/variant-pipeline/share/trimmomatic-0.40-0/adapters/TruSeq3-PE.fa"
 
 mkdir -p "$TRIMMED_DIR" "$QC_DIR/raw" "$QC_DIR/trimmed"
 
@@ -14,20 +14,10 @@ echo "[QC] Running FastQC on raw reads..."
 fastqc "$RAW_R1" "$RAW_R2" --outdir "$QC_DIR/raw" --threads "$THREADS"
 
 echo "[QC] Running Trimmomatic..."
-trimmomatic PE -threads "$THREADS" \
-  "$RAW_R1" "$RAW_R2" \
-  "$TRIMMED_DIR/NA12878_R1_trimmed.fastq.gz" \
-  "$TRIMMED_DIR/NA12878_R1_unpaired.fastq.gz" \
-  "$TRIMMED_DIR/NA12878_R2_trimmed.fastq.gz" \
-  "$TRIMMED_DIR/NA12878_R2_unpaired.fastq.gz" \
-  ILLUMINACLIP:"$ADAPTER":2:30:10:2:keepBothReads \
-  LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 \
-  2> logs/trimmomatic.log
+trimmomatic PE -threads "$THREADS"   "$RAW_R1" "$RAW_R2"   "$TRIMMED_DIR/NA12878_R1_trimmed.fastq.gz"   "$TRIMMED_DIR/NA12878_R1_unpaired.fastq.gz"   "$TRIMMED_DIR/NA12878_R2_trimmed.fastq.gz"   "$TRIMMED_DIR/NA12878_R2_unpaired.fastq.gz"   ILLUMINACLIP:"$ADAPTER":2:30:10:2:keepBothReads   LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36   2> logs/trimmomatic.log
 
 echo "[QC] Running FastQC on trimmed reads..."
-fastqc "$TRIMMED_DIR/NA12878_R1_trimmed.fastq.gz" \
-       "$TRIMMED_DIR/NA12878_R2_trimmed.fastq.gz" \
-       --outdir "$QC_DIR/trimmed" --threads "$THREADS"
+fastqc "$TRIMMED_DIR/NA12878_R1_trimmed.fastq.gz"        "$TRIMMED_DIR/NA12878_R2_trimmed.fastq.gz"        --outdir "$QC_DIR/trimmed" --threads "$THREADS"
 
 echo "[QC] Generating MultiQC report..."
 multiqc "$QC_DIR" --outdir "$QC_DIR" --filename multiqc_report
